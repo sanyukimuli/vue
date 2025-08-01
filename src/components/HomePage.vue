c3e9b98c9287d5472406bb29567e536e4068c27b
Sanyu Kimuli <sanyukimuli@gmail.com>
2025-08-01 08:34:17 +0100

    first commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>
+            Welcome to Kampala
+        </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+createApp(App).mount('#app') //this is a selctor that selects the div with id app that is in index.html and it takes in App which is a vue js component as specified on line 3
+
+//our focu is now not on index.html or main.js but instead app.vue
